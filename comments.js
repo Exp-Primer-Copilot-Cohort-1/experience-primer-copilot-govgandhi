a78ed41c88071985be308cc435ed7a6ca8aeb9e5
Govind <51152633+govgandhi@users.noreply.github.com>
2023-12-11 19:35:10 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,2 +1,11 @@
-//create web server with express 
-const express = require('express');
+const http = require('http');
+// Create web server
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!');
+});
+
+server.listen(3000, 'localhost', () => {
+    console.log('Server running at http://localhost:3000/');
+});
